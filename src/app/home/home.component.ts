e57720c2602313b295ecfa72efb9477a5c273a4d
Shishira <shishira.ms@fyle.in>
2022-01-12 16:41:08 +0530

    added products 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,9 @@ export class HomeComponent implements OnInit {
   description: string = 'Welcome to KKR';
   showNav: boolean = false;
   showLeft: boolean = false;
-  showright: boolean = true;
+  showRight: boolean = true;
+  showHeaderButtons: boolean = true;
+  showHeader: boolean = true;
   ngOnInit(): void {
   }
 
@@ -23,7 +25,6 @@ export class HomeComponent implements OnInit {
     const scrollValue = slideShowContainer?.scrollLeft || 0;
     $(".slide-show-container").scrollLeft(scrollValue + 300);
     this.imgIndex++;
-    // this.description = this.setProductName(slideShowContainer?.scrollLeft || 0 ,this.imgIndex)
   };
 
   scrollPrevious () {
@@ -42,14 +43,11 @@ export class HomeComponent implements OnInit {
         const scrolly = app.scrollTop;
         const sectionHeight = section1 && section1.scrollHeight || 0;
         const stepper = sectionHeight/10;
-
-        console.log('scrollY: ', scrollY, sectionHeight);
         if (scrolly > (sectionHeight - 1)) {
           this.showNav = true;
         } else {
           this.showNav = false;
         }
-        
         if (scrolly > stepper) {
           $(".welcome-text").css("opacity", "0.9");
           $(".btn-order-online-container").css("opacity", "0.9");
@@ -71,13 +69,19 @@ export class HomeComponent implements OnInit {
           $(".btn-order-online-container").css("opacity", "0.1");
           $(".header").css("color", "floralwhite");
           $(".description").css("color", "orange");
+          this.showHeader = true;
           this.header = 'Our Products';
           this.description = 'Gulaab Jamoon';
         } else if (scrolly < (stepper * 5)) {
+          this.showHeader = true;
           this.header = 'Hola!';
           this.description = 'Welcome to KKR';
           $(".header").css("color", "darkblue");
           $(".description").css("color", "black");
+        }
+        if (scrolly > ((stepper * 5) + sectionHeight)) {
+          this.header = 'Our Products 2';
+          this.description = 'Cheese ball';
         }
         if (scrolly > (stepper * 6)) {
           $(".welcome-text").css("opacity", "0.3");
@@ -99,57 +103,112 @@ export class HomeComponent implements OnInit {
           $(".welcome-text").css("opacity", "1.0");
           $(".btn-order-online-container").css("opacity", "1.0");
         }
+
+        if (scrolly >= ((sectionHeight * 3) - 140)) {
+          this.showHeaderButtons = false;
+          this.showNav = false;
+        }
+        if (scrolly < ((sectionHeight * 3) - 140)) {
+          this.showHeaderButtons = true;
+          this.showNav = true;
+        }
       });
 
-      const slideShowContainer = document.getElementById("slide-show-container");
+      const slideShowContainer = document.getElementById("slide-show-container1");
       const image1 = document.getElementById("image-one");
       slideShowContainer && slideShowContainer.addEventListener('scroll', () => {
         const scrollX = slideShowContainer.scrollLeft;
         const imageWidth = image1?.scrollWidth || 0;
-        console.log('scrollx: ', scrollX);
-        console.log('image2 width: ', image1?.scrollWidth);
 
         if ((scrollX === 0)) {
           this.description = 'Gulaab Jamoon';
           this.showLeft = false;
-          this.showright = true;
+          this.showRight = true;
+          this.showHeader = true;
         }
 
         if ((scrollX > 0) && (scrollX <= imageWidth)) {
-          this.description = 'Product 2';
+          this.description = 'Chakli';
           this.showLeft = false;
-          this.showright = true;
+          this.showRight = true;
+          this.showHeader = false;
         }
 
         if (scrollX > (imageWidth) && scrollX <= (imageWidth*2)) {
-          this.description = 'Product 3';
+          this.description = 'Jeera powder';
           this.showLeft = true;
-          this.showright = true;
+          this.showRight = true;
         }
         if (scrollX > (imageWidth*2) && scrollX >= (imageWidth*3)) {
-          this.description = 'Product 4';
+          this.description = 'Rasam powder';
           this.showLeft = true;
-          this.showright = true;
+          this.showRight = true;
         }
         if (scrollX > (imageWidth*3) && scrollX >= (imageWidth*4)) {
-          this.description = 'Product 5';
+          this.description = 'Dhaniya Powder';
           this.showLeft = true;
-          this.showright = true;
+          this.showRight = true;
         }
         if (scrollX > (imageWidth*4) && scrollX >= (imageWidth*5)) {
-          this.description = 'Product 6';
+          this.description = 'Sambar powder for Idli';
           this.showLeft = true;
-          this.showright = true;
+          this.showRight = true;
         }
         if (scrollX > (imageWidth*5) && scrollX >= (imageWidth*6)) {
-          this.description = 'Product 7';
+          this.description = 'Bisibelebath powder';
           this.showLeft = true;
-          this.showright = true;
+          this.showRight = true;
         }
         if (scrollX > (imageWidth*6) && scrollX >= (imageWidth*7)) {
-          this.description = 'Product 8';
+          this.description = 'Vangibath Powder';
           this.showLeft = true;
-          this.showright = false;
+          this.showRight = false;
+        }
+      });
+
+      const slideShowContainer2 = document.getElementById("slide-show-container2");
+      slideShowContainer2 && slideShowContainer2.addEventListener('scroll', () => {
+        const scrollX = slideShowContainer2.scrollLeft;
+        const imageWidth = image1?.scrollWidth || 0;
+
+        if ((scrollX === 0)) {
+          this.description = 'Cheese Ball';
+          this.showLeft = false;
+          this.showRight = true;
+          this.showHeader = true;
+        }
+
+        if ((scrollX > 0) && (scrollX <= imageWidth)) {
+          this.description = 'Grilled garlic Paneer';
+          this.showLeft = false;
+          this.showRight = true;
+          this.showHeader = false;
+        }
+
+        if (scrollX > (imageWidth) && scrollX <= (imageWidth*2)) {
+          this.description = 'BBQ Paneer';
+          this.showLeft = true;
+          this.showRight = true;
+        }
+        if (scrollX > (imageWidth*2) && scrollX >= (imageWidth*3)) {
+          this.description = 'Peas cutlet';
+          this.showLeft = true;
+          this.showRight = true;
+        }
+        if (scrollX > (imageWidth*3) && scrollX >= (imageWidth*4)) {
+          this.description = 'Masala Dosa';
+          this.showLeft = true;
+          this.showRight = true;
+        }
+        if (scrollX > (imageWidth*4) && scrollX >= (imageWidth*5)) {
+          this.description = 'Sambar powder for Idli';
+          this.showLeft = true;
+          this.showRight = true;
+        }
+        if (scrollX > (imageWidth*5) && scrollX >= (imageWidth*6)) {
+          this.description = 'Bisibelebath powder';
+          this.showLeft = true;
+          this.showRight = true;
         }
       });
     })()
